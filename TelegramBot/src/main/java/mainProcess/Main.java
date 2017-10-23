3e2e1e5b218cc873dc13b6b58cc4b367986c423a
sausmagic <umbertopalo@hotmail.it>
2017-10-23 23:23:51 +0200

    piccole fix

--- TelegramBot/src/main/java/mainProcess/Main.java
+++ TelegramBot/src/main/java/mainProcess/Main.java
@@ -22,11 +22,16 @@ public class Main {
 
 	
 	public static void main(String[] args) throws IOException {
-		 String filename = "tmp/ferrari.txt";
-		// String filename = "tmp/girl.txt";
+//		 String filename = "tmp/ferrari.txt";
+//		 String filename = "tmp/girl.txt";
 		// String category = Group.Category.GIRL.getCategoryName();
 
-		 getAllImages(Group.Category.GIRL);
+		 List<Image> images = getAllImages(Group.Category.ALL);
+		 for (Image image : images) {
+			if(image.getUrl().endsWith(".svg")) {
+				System.out.println(image);
+			}
+		}
 //		 loadImages(filename, mainProces, Group.Category.AUTO.getCategoryName());
 		// deleteImageToDb(Group.Category.GIRL.getCategoryName());
 
@@ -38,10 +43,11 @@ public class Main {
 		userFactory.removeAllUsers();
 	}
 
-	private static void getAllImages(Category category) {
+	private static List<Image> getAllImages(Category category) {
 		for (Image image : factoryImage.getAllImages(category)) {
 			System.out.println(image);
 		}
+		return factoryImage.getAllImages(category);
 	}
 
 	private static void deleteImageToDb(String categoryName) {
